3a434edf8023cbde9feffeeada458851bca972a2
Benji Huser-Berta <benj.huser@gmail.com>
2024-06-08 11:21:37 +0200

    increase version to 0.9.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flowmetricscsv',
-    version='0.1.15',
+    version='0.9.0',
     packages=find_packages(),
     include_package_data=True,
     package_data={
